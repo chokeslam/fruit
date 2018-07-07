ed47c59722440c0f06ba2ac3768a6dbae203477f
chokeslam <chokeslam5608@gmail.com>
2018-07-07 18:10:55 +0800

    add packeage and routing

---
+++ src/fruit/FruitPackage.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Part of fruit project.
+ *
+ * @copyright  Copyright (C) 2018 ${ORGANIZATION}.
+ * @license    __LICENSE__
+ */
+
+namespace fruit;
+
+use Windwalker\Core\Package\AbstractPackage;
+
+/**
+ * The FruitPackage class.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class FruitPackage extends AbstractPackage
+{
+    /**
+     * boot
+     *
+     * @return  void
+     *
+     * @throws \ReflectionException
+     * @throws \Windwalker\DI\Exception\DependencyResolutionException
+     *
+     * @since  __DEPLOY_VERSION__
+     */
+    public function boot()
+    {
+        return parent::boot();
+    }
+}
